ce831297d4737ade9f60e8bd8ea03dc00fa1f76d
Pedro Nauck <pedronauck@gmail.com>
2024-03-05 21:49:24 -0300

    chore: add chain info

--- src/constants.rs
+++ src/constants.rs
@@ -8,7 +8,7 @@ pub const WALLET_SECRET_KEY: &str = "WALLET_SECRET_KEY";
 pub const CLERK_KEY: &str = "CLERK_KEY";
 pub const PUBLIC_FUEL_NODE_URL: &str = "PUBLIC_FUEL_NODE_URL";
 pub const WALLET_SECRET_DEV_KEY: &str =
-    "99ad179d4f892ff3124ccd817408ff8a4452d9c16bb1b4968b8a59797e13cd7a";
+    "a449b1ffee0e2205fa924c6740cc48b3b473aa28587df6dab12abc245d1f5298";
 pub const FUEL_NODE_URL: &str = "FUEL_NODE_URL";
 pub const DEFAULT_NODE_URL: &str = "http://127.0.0.1:4000";
 pub const DISPENSE_AMOUNT: &str = "DISPENSE_AMOUNT";
@@ -17,7 +17,7 @@ pub const DEFAULT_DISPENSE_INTERVAL: u64 = 24 * 60 * 60;
 pub const DEFAULT_FAUCET_DISPENSE_AMOUNT: u64 = 10_000_000;
 pub const FAUCET_ASSET_ID: AssetId = AssetId::new([0; 32]);
 pub const SERVICE_PORT: &str = "PORT";
-pub const DEFAULT_PORT: u16 = 3000;
+pub const DEFAULT_PORT: u16 = 3002;
 
 pub const MIN_GAS_PRICE: &str = "MIN_GAS_PRICE";
 pub const TIMEOUT_SECONDS: &str = "TIMEOUT_SECONDS";
